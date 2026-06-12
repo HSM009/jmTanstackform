6bd703e61b2b580e1a43ef2e1432acc6078c0dc3
Hosein Sirat Mohammad <hosein.sirat@pralstp.org>
2026-06-12 17:29:25 +0500

    backend fixes

--- src/data/session.ts
+++ src/data/session.ts
@@ -1,24 +1,24 @@
 import { auth } from '@/lib/auth'
-import { redirect } from '@tanstack/react-router'
 import { createServerFn } from '@tanstack/react-start'
-import { getRequestHeaders } from '@tanstack/react-start/server'
-import { getEvent, setCookie } from 'vinxi/server' // or 'nitropack/runtime'
+import {
+  getRequestHeaders,
+  setResponseHeader,
+} from '@tanstack/react-start/server'
 
 export const getSessionFn = createServerFn({ method: 'GET' }).handler(
   async () => {
     const headers = getRequestHeaders()
+
     const session = await auth.api.getSession({ headers })
-    const token = 'your_geerated_jwt_toke'
-    const event = getEvent()
-    setCookie(event, 'session_token', token, {
-      httpOnly: true,
-      secure: true, // Required for HTTPS production environments on Vercel
-      sameSite: 'lax',
-      path: '/',
-    })
+
+    const token = 'your_gen11erated_jwt_token'
+    const cookieValue = `session_token=${token}; HttpOnly; Secure; SameSite=Lax; Path=/`
+    setResponseHeader('Set-Cookie', cookieValue)
+
     if (!session) {
-      throw redirect({ to: '/' })
+      return { user: null }
     }
+
     return session
   },
 )
